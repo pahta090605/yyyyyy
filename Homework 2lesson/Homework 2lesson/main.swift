47b927944fb11f20c7580692c236daf24c514230
Zubaidillo Jalilov <zubai.2005@gmail.com>
2022-09-20 01:01:25 +0600

    noun comments

---
+++ Homework 2lesson/Homework 2lesson/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Homework 2lesson
+//
+//  Created by mac on 20/9/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ homework 1lesson/homework 1lesson/main.swift
@@ -0,0 +1,79 @@
+//
+//  main.swift
+//  homework 1lesson
+//
+//  Created by mac on 19/9/22.
+//
+
+import Foundation
+
+let name: String = "zubai"
+var age: Int = 18
+print("name: \(name),age \(age)")
+
+let name1: String = "askat"
+ print("name: \(name),age \(age)")
+let number3: Int = -24
+var number1: Int = 18
+let number2: String = "рврп"
+var age1: Int = 19
+
+
+let country: String = "America"
+var amountofpeople: Int = 680000
+  print("country: \(country) \(amountofpeople)")
+   
+
+let avto: String = "lada"
+var yearMade: Int = 1997
+print("avto: \(avto) \(yearMade)")
+  
+
+let avto1: String = "yoyota"
+var  yearMade1: Int = 2027
+print("avto: \(avto1) \(yearMade)")
+
+let city: String = "osh"
+var year: Int = 2020
+print("city: \(city) \(year)")
+
+let name2: String = "Abubakir"
+var thisYear: Int = 2022
+var birthYear: Int = 2001
+print("name: \(name2),age: \(thisYear - birthYear)")
+
+
+let name4: String = "zubaidullo"
+let surname: String = "jalilov"
+var age3: Int = 18
+var town: String = "Hollywood"
+var phoneNumber: String = "0500269616"
+
+print("surname: \(surname) \(name4),age \(age3)")
+print("city: \(town)")
+print("phoneNumber: \(phoneNumber)")
+
+ 
+print("напиши число")
+var number = Int(readLine()!)!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
