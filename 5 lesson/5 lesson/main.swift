ae3041ab973d53eb06f5c37bec745316d433deaa
Zubaidillo Jalilov <zubai.2005@gmail.com>
2022-09-22 19:08:25 +0600

    lesson 1

---
+++ 5 lesson/5 lesson/main.swift
@@ -0,0 +1,12 @@
+
+ let readline = readLine()!
+ 
+
+
+
+
+
+
+
+
+
--- Homework 2lesson/Homework 2lesson/main.swift
+++ Homework 2lesson/Homework 2lesson/main.swift
@@ -7,5 +7,44 @@
 
 import Foundation
 
-print("Hello, World!")
 
+print("Напиши страны")
+
+func Turkey1(){
+   print("weather in Turkey todey is 33 ")
+}
+func Turkey2(){
+    print("weather in Turkey tomorrow is 35")
+}
+func Turkey3(){
+    print("weather in Turkey after 2 deys is 33")
+}
+func Russia1(){
+    print("weather in Russia todey is 53 ")
+}
+func Russia2(){
+   print("weather in Russia c58")
+}
+func Russia3(){
+   print("weather in Russia after 2 deys is 53")
+}
+func America1(){
+   print("weather in America todey is 17")
+}
+func America2(){
+    print("weather in America weather in 20")
+}
+func America3(){
+    print("weather in America after 2 deys is 17")
+}
+var capital = readLine()
+
+switch capital {
+case "capital"
+     Turkey1()
+     Turkey1()
+    
+    
+    
+    
+    
